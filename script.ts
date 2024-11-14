d7c4f119f92ba9c10f574ce675b8737f331833c7
Daniel-derf <danielpardinho321@gmail.com>
2024-11-13 21:44:27 -0300

    initial setup

---
+++ script.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+// use `prisma` in your application to read and write data in your DB
+
+async function main(){
+
+}
+
+main()
+  .catch(e => {
+    console.error(e.message)
+  })
+  .finally(async() => {
+    await prisma.$disconnect()
+  })
